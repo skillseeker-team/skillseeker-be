bc849b77374d6c5d801d4ac38d671434e36544da
SUHYUN YU <suhyun1219yu@gmail.com>
2026-02-07 01:05:05 +0900

    chore: initial commit

---
+++ src/test/java/com/example/skillseeker_be/SkillseekerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skillseeker_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillseekerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
